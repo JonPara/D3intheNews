9dd4b70aa563561b7b3a711e25eed6785020804c
Jonathan Paradiso <33237727+JonPara@users.noreply.github.com>
2018-05-26 23:46:43 -0400

    Created axes. Appended text and circles

--- Data Journalism and D3/app.js
+++ Data Journalism and D3/app.js
@@ -40,6 +40,38 @@ d3.csv("usData.csv", function(error, healthData){
     var yLinearScale = d3.scaleLinear().range([height, 0]);
 
     // Create axis functions
-    var bottomAxis = d3.axisBottom(xLinearScale)
+    var bottomAxis = d3.axisBottom(xLinearScal);
+    var leftAxis = d3.axesLeft(yLinearScale)
 
-})
+    // Append axes to chart
+    chartGroup.append("g")
+              .attr("transform", `translate(0, ${height})`)
+              .call(bottomAxis);
+
+    chartGroup.append("g")
+              .call(leftAxis);
+
+    // Create circles
+    var circlesGroup = chartGroup.selectAll("circle")
+                                 .data(healthData)
+                                 .enter()
+                                 .append("circle")
+                                 .attr("cx", d => xLinearScale(d.length))
+                                 .attr("cy", d => yLinearScale(d.hits))
+                                 .attr("r", "15")
+                                 .attr("fill", "blue")
+                                 .attr("opacity", ".5")
+
+
+    chartGroup.append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 0 - margin.left + 40)
+              .attr("x", 0 - (height / 2))
+              .attr("dy", "1em")
+              .attr("class", "axisText")
+              .text("")
+
+    chartGroup.append("text")
+              .attr("transform", `translate(${width / 2}, ${height + margin.top + 37})`)
+              
+});
